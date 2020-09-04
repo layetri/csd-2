84e18079bf03794d912bce14e937aba47bb12959
layetri <daniel@mainstreamct.com>
2020-09-04 10:41:42 +0200

    Updated playsound.py with advance tasks

--- csd-2a/python-basics/playsound.py
+++ csd-2a/python-basics/playsound.py
@@ -1,14 +1,22 @@
 import simpleaudio as sa
 
 # Give the user a heads-up that the sound is, in fact, playing
-print("Song's starting")
+print("Hey!")
+# Ask for input
+# bpm = input("give bpm: ")
+times = input("rEpeAT h0W mUcH?? ")
+
+# Calculate interval
+# interval = 60000 / int(bpm)
 
 # Load the wave into a WaveObject
-wave = sa.WaveObject.from_wave_file("../../assets/testsong.wav")
-# Play the sound
-play = wave.play()
-# Sleep till done
-play.wait_done()
+wave = sa.WaveObject.from_wave_file("../../assets/testsample.wav")
+
+for i in range(int(times)):
+    # Play the sound
+    play = wave.play()
+    # Sleep till done
+    play.wait_done()
 
 # Done!
 print("Song's over")
