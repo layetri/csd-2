d5c73ec83d57b1ab9a8c82fa85b4ccc15eeba96e
layetri <daniel@mainstreamct.com>
2020-09-04 10:46:10 +0200

    added bpm to playsound.py

--- csd-2a/python-basics/playsound.py
+++ csd-2a/python-basics/playsound.py
@@ -1,13 +1,14 @@
 import simpleaudio as sa
+import time as t
 
 # Give the user a heads-up that the sound is, in fact, playing
 print("Hey!")
 # Ask for input
-# bpm = input("give bpm: ")
+bpm = input("give bpm: ")
 times = input("rEpeAT h0W mUcH?? ")
 
 # Calculate interval
-# interval = 60000 / int(bpm)
+interval = 60 / int(bpm)
 
 # Load the wave into a WaveObject
 wave = sa.WaveObject.from_wave_file("../../assets/testsample.wav")
@@ -15,8 +16,10 @@ wave = sa.WaveObject.from_wave_file("../../assets/testsample.wav")
 for i in range(int(times)):
     # Play the sound
     play = wave.play()
-    # Sleep till done
-    play.wait_done()
+    # Sleep till next tick
+    t.sleep(interval)
+    # Stop sound
+    play.stop()
 
 # Done!
 print("Song's over")
