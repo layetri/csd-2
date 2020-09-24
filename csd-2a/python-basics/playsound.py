c2ca5f3bf09e19720e8e08359aef50d3593aaf81
layetri <daniel@mainstreamct.com>
2020-09-24 11:08:18 +0200

    made playsound.py fancier

--- csd-2a/python-basics/playsound.py
+++ csd-2a/python-basics/playsound.py
@@ -1,25 +1,33 @@
 import simpleaudio as sa
 import time as t
 
-# Give the user a heads-up that the sound is, in fact, playing
-print("Hey!")
+# Set up environment
+play = None
+start = None
+
 # Ask for input
 bpm = input("give bpm: ")
 times = input("rEpeAT h0W mUcH?? ")
+playing = True
 
 # Calculate interval
-interval = 60 / int(bpm)
+interval = 60000 / int(bpm)
 
 # Load the wave into a WaveObject
 wave = sa.WaveObject.from_wave_file("../../assets/testsample.wav")
 
-for i in range(int(times)):
+
+def spawn_wave_on_main_thread():
+    global play
+    global start
+    start = t.time()
     # Play the sound
     play = wave.play()
-    # Sleep till next tick
-    t.sleep(interval)
-    # Stop sound
-    play.stop()
+
+
+while playing:
+    if start is not None and t.time() > int(start) + interval:
+        spawn_wave_on_main_thread()
 
 # Done!
 print("Song's over")
