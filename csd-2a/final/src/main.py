ffb01c0444759dc1315f2370d6eaf806e8b08b99
layetri <daniel@mainstreamct.com>
2020-10-17 22:36:27 +0200

    Final-2a: added non-graphical user interface.

--- csd-2a/final/src/main.py
+++ csd-2a/final/src/main.py
@@ -1,29 +1,19 @@
-import playback, ui, algorithm, train
-import multiprocessing
-import midiutil
-import mido
+import ui
+from termcolor import colored
 
-# Start by training the model
-# Note that this only trains a model when no previous model is found.
-algorithm.train()
+# Example Usage:
+# Train a model for a specified
+# => train.train((6, 8), 'example')
 # Use the previously trained model to generate a rhythm
-rhythm = algorithm.gen((5, 4), 16, 4)
+# => rhythm = algorithm.gen((6, 8), 16, 4)
 
 # Convert the generated rhythm to timestamps
 # playback.extract()
 # playback.play()
 
-# ui_proc = multiprocessing.Process(ui.gather_input)
-
-
-def handle_input(command):
-    print(command)
-    if command == "play":
-        playback.play()
-    elif command == "help":
-        print('Help options include:')
-
+print('Welcome to', colored('NeuroBeat', 'cyan') + '!')
+print('Type', colored('help', 'yellow'), 'for a list of possible commands.\n')
 
 while True:
     cmd = input("Command: ")
-    handle_input(cmd)
+    ui.handle_input(cmd)
--- csd-2a/final/src/ui.py
+++ csd-2a/final/src/ui.py
@@ -1,3 +1,213 @@
-def gather_input():
-    # Gather user input
-    print('input!!!')
+import playback, ui, algorithm, train
+from termcolor import colored
+import math
+import os
+
+generated = {}
+sig = (4, 4)
+bpm = 120
+division = 16
+
+
+# Ask the user for a time signature
+def time_sig_input():
+    global sig
+    sig_set = None
+
+    print('What', colored('time signature', 'yellow'), 'do you want to use? (Default is ' + str(sig[0]) + '/' + str(sig[1]) + ')')
+
+    while type(sig_set) is not tuple:
+        sig_set = input(' => ')
+        if not sig_set:
+            sig_set = sig
+            print(colored('√', 'green'), 'Using default time signature')
+        else:
+            try:
+                # Parse the time signature to a tuple
+                sig_set = sig_set.split('/', 2)
+                sig_set = (int(sig_set[0]), int(sig_set[1]))
+
+                # Make sure that the denomination has a valid value
+                if not math.log2(sig_set[1]).is_integer():
+                    raise AssertionError
+            except ValueError:
+                sig_set = None
+                print(colored('×', 'red'), "Error: could not parse input to tuple. Try again.")
+            except AssertionError:
+                sig_set = None
+                print(colored('×', 'red'), "Error: not a valid denomination. Try again.")
+
+    if sig_set and sig_set is not sig:
+        sig = sig_set
+
+
+# Ask the user to pick a dataset
+def series_input(context):
+    # Ask the user to pick a dataset
+    series = "example"
+    series_set = None
+
+    print('What', colored('dataset', 'yellow'), 'do you want to use? (Default is "example")')
+
+    while type(series_set) is not str:
+        series_set = input(' => ')
+
+        if not series_set:
+            series_set = series
+
+        # Check if the series directory exists for the desired action
+        if not os.path.isdir('../' + context + '/' + series_set + '/' + str(sig[0]) + '-' + str(sig[1])):
+            series_set = None
+            print(colored('×', 'red'), "Error: model not found. Try again.")
+
+    if series_set is not series:
+        series = series_set
+
+    return series
+
+
+# Ask the user for the division
+def division_input():
+    global division
+    division_set = None
+
+    print('What', colored('subdivision', 'yellow'), 'do you want to use? (Default is', str(division) + 'th notes)')
+
+    while type(division_set) is not int:
+        division_set = input(' => ')
+        if not division_set:
+            division_set = division
+        else:
+            try:
+                # Parse the division to a number
+                division_set = int(division_set)
+
+                # Make sure that the division has a valid value
+                if not math.log2(division_set).is_integer():
+                    raise AssertionError
+            except ValueError:
+                division_set = None
+                print(colored('×', 'red'), "Error: could not parse input to number. Try again.")
+            except AssertionError:
+                division_set = None
+                print(colored('×', 'red'), "Error: not a valid subdivision. Try again.")
+
+    if division_set is not division:
+        division = division_set
+
+
+# Ask the user for the length
+def length_input():
+    length = 4
+    length_set = None
+
+    print('What should the', colored('length', 'yellow'), 'of the rhythm be? (Default is 4 measures)')
+
+    while type(length_set) is not int:
+        length_set = input(' => ')
+        if not length_set:
+            length_set = length
+        else:
+            try:
+                # Parse the length to a number
+                length_set = int(length_set)
+            except ValueError:
+                length_set = None
+                print(colored('×', 'red'), "Error: could not parse input to number. Try again.")
+
+    if length_set is not length:
+        length = length_set
+
+    return length
+
+
+# Ask the user to pick an evolve option
+def evolve_input():
+    evolve = False
+    evolve_set = None
+
+    print('Should the generated rhythm', colored('evolve', 'yellow') + '? (Default is No)')
+
+    while type(evolve_set) is not bool:
+        evolve_set = input(' => ')
+
+        # Parse the value to a bool and assert it is valid
+        if not evolve_set:
+            evolve_set = evolve
+        elif evolve_set == "yes":
+            evolve_set = True
+        elif evolve_set == "no":
+            evolve_set = False
+        else:
+            evolve_set = None
+            print(colored('×', 'red'), "Error: input is neither \"yes\" or \"no\". Try again.")
+
+    if evolve_set is not evolve:
+        evolve = evolve_set
+
+    return evolve
+
+
+def do_train():
+    print(colored('Initializing training...', 'cyan'))
+
+    # Ask the user for a time signature
+    time_sig_input()
+    # Ask the user for a dataset to use
+    series = series_input('train')
+
+    # Start the training
+    print(colored('\nStarting training...', 'cyan'))
+    train.train(sig, series)
+
+
+def do_generate():
+    global generated
+
+    print(colored('Initializing rhythm generation...', 'cyan'))
+    # Ask the user for a time signature
+    time_sig_input()
+
+    # Ask the user for the division
+    division_input()
+
+    # Ask the user for the length
+    length = length_input()
+
+    # Ask the user to pick a dataset
+    series = series_input('model')
+
+    # Ask the user to pick an evolve option
+    evolve = evolve_input()
+
+    # Generate a rhythm with the user input and store it
+    generated = algorithm.gen(sig, division, length, series, evolve)
+
+
+def do_play():
+    playback.play()
+
+
+def handle_input(command):
+    if command == "play":
+        # Guide the user to play back the generated rhythm
+        do_play()
+    elif command == "train":
+        # Guide the user to train a model
+        do_train()
+    elif command == 'generate':
+        # Guide the user to generate a new rhythm
+        do_generate()
+    elif command == "help":
+        # Print the help page
+        print('The following commands are available for you to use:')
+        print(colored('train', 'cyan'), '        Train a model for a specified dataset.')
+        print(colored('generate', 'cyan'), '     Generate a rhythm using a specified model.')
+        print(colored('play', 'cyan'), '         Play back the generated rhythm.')
+        print(colored('export', 'cyan'), '       Write the generated rhythm to a MIDI file.')
+    else:
+        # Handle unknown commands
+        print('Unknown command. Type', colored('help', 'yellow'), 'for a list of included commands.')
+
+    # Print a blank line after finishing
+    print('')
