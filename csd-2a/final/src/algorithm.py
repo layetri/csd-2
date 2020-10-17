241050fe14fc2c4daec81a93d7257033a7ddd4cc
layetri <daniel@mainstreamct.com>
2020-10-17 22:34:08 +0200

    Final-2a: added generation algorithm (unoptimized but basics are there).

--- csd-2a/final/src/algorithm.py
+++ csd-2a/final/src/algorithm.py
@@ -1,33 +1,41 @@
-beats = 5
-measure = [['kick', 'hat'], ['hat'], ['snare', 'hat'], ['hat'], ['hat']]
-options = ['kick', 'snare', 'hat']
-weighed = {}
-significance = []
+import pickle
+import random
 
 
-def train():
-    global weighed, significance
-
-    for beat in measure:
-        significance.append(len(beat) / beats)
-
-        for option in options:
-            if option in weighed:
-                weighed[option] += beat.count(option)
-            else:
-                weighed[option] = beat.count(option)
-
-    for el in weighed:
-        weighed[el] = 1 - (weighed[el] / beats)
-
-    print(weighed)
-    print(significance)
-
-
-def interp():
-    print('now interpolating')
-
-
-def gen(signature, division, length, evolve=False):
+def gen(signature, division, length, series="example", evolve=False):
     # Generate a rhythm from the trained model
-    print(signature, division, length, evolve)
+    model = pickle.load(open('../model/'+series+'/'+str(signature[0])+'-'+str(signature[1])+'/sum.pickle', 'rb'))
+
+    # Prepare dependencies for evolving generation
+    if evolve:
+        layer = pickle.load(open('../model/'+series+'/'+str(signature[0])+'-'+str(signature[1])+'/overview.pickle', 'rb'))
+        average = {}
+
+    ppq = 48
+    increment = ppq / (division / (32 / signature[1]))
+    slices = division * signature[0] * length
+    single_measure = division * signature[0]
+
+    # Prepare a dictionary for the result
+    result = {}
+
+    for i in range(slices):
+        pos = i % single_measure
+        if pos in model:
+            print(i, ':', model[pos])
+            if i not in result:
+                result[i] = []
+
+            if evolve:
+                if i in layer:
+                    average[i] = {}
+                    for note in model[pos]:
+                        if note in layer[i]:
+                            if random.random() < (model[pos][note] + layer[i][note]) / 2:
+                                result[i].append(note)
+            else:
+                for note in model[pos]:
+                    if random.random() < model[pos][note]:
+                        result[i].append(note)
+
+    print(result)
