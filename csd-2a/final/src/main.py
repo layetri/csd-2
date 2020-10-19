64f27364fffa0f7d1d97a686779d4bdca928593f
layetri <daniel@mainstreamct.com>
2020-10-20 01:31:27 +0200

    Final-2a: removed obsolete pygame flag. Added comments.

--- csd-2a/final/src/main.py
+++ csd-2a/final/src/main.py
@@ -1,20 +1,19 @@
 import ui
 from termcolor import colored
 
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-
 # Example Usage:
 # Train a model on a specified dataset
 # => train.train((6, 8), 'example')
 # Use the previously trained model to generate a rhythm
 # => rhythm = algorithm.gen((6, 8), 16, 4)
-# Play back the generated rhythm
-# => playback.play()
+# Provide playback controls for the generated rhythm
+# => playback.init(rhythm)
 
+# Welcome the user~ (with tea or something, idk)
 print('Welcome to', colored('NeuroBeat', 'cyan') + '!')
 print('Type', colored('help', 'yellow'), 'for a list of possible commands.\n')
 
+# Let the party begin!
 while True:
     cmd = input("Command: ")
     ui.handle_input(cmd)
